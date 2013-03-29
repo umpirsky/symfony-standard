fccec5b099d0e9d6dd5d9569636e86ff3464187d
umpirsky <umpirsky@gmail.com>
2013-03-29 17:30:31 +0100

    Symfony Settings Edition

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,9 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Liip\DoctrineCacheBundle\LiipDoctrineCacheBundle(),
+            new Sylius\Bundle\ResourceBundle\SyliusResourceBundle(),
+            new Sylius\Bundle\SettingsBundle\SyliusSettingsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
